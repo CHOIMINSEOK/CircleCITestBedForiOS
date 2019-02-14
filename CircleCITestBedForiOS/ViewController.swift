1d2d0b8eadb7fd4d133df6376e6806b5dc67db67
Unknown <cms3718@gmail.com>
2019-02-14 16:01:59 +0900

    Init Project

---
+++ CircleCITestBedForiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleCITestBedForiOS
+//
+//  Created by Minseok Choi on 14/02/2019.
+//  Copyright © 2019 Minseok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
